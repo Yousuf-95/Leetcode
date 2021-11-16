cf8b57ee408b2083134a1fb07df72c39b1a2aecb
Yousuf Suhail <yf.yousuf95@gmail.com>
2021-11-16 22:10:04 +0530

    234. Palindrome Linked List - Solution

---
+++ 234-palindromeLinkedList/palindromeLinkedList.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let bucket = [];
+    while(head) {
+        bucket.push(head.val);
+        head = head.next;
+    }
+    return bucket.join('') === bucket.reverse().join('') ? true : false;
+};
